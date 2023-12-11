c4ae896d7456a8256eebace5b9a006b2c099af44
Osama Ragab <osamaragab520@gmail.com>
2023-12-11 13:26:22 +0200

    Add DialTimeout function and test
    This commit adds a new function called DialTimeout to the connections package. The function allows for establishing a network connection with a specified timeout. It also includes a corresponding test to ensure the timeout behavior is correctly handled.

---
+++ connections/dial_timeout_test.go
@@ -0,0 +1,38 @@
+package connections
+
+import (
+	"net"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func DialTimeout(network string, address string, timeout time.Duration) (net.Conn, error) {
+	d := net.Dialer{
+		Control: func(_, address string, _ syscall.RawConn) error {
+			return &net.DNSError{
+				Err:         "connection timed out",
+				Name:        address,
+				Server:      "localhost:8080",
+				IsTimeout:   true,
+				IsTemporary: true}
+		},
+		Timeout: timeout}
+
+	return d.Dial(network, address)
+}
+func TestDialTimeout(t *testing.T) {
+	c, err := DialTimeout("tcp", "10.0.0.1:http", 5*time.Second)
+	if err == nil {
+		c.Close()
+		t.Fatal("Connection did not time out")
+	}
+
+	nErr, ok := err.(net.Error)
+	if !ok {
+		t.Fatal(err)
+	}
+	if !nErr.Timeout() {
+		t.Fatal("Error is not a timeout")
+	}
+}
