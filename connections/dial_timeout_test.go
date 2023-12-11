a2ef5c6e648fb9c67f0fa36a9ffd1d640d47bf1f
Osama Ragab <osamaragab520@gmail.com>
2023-12-11 16:36:13 +0200

    Add DialContext test for timeout handling using Context

--- connections/dial_timeout_test.go
+++ connections/dial_timeout_test.go
@@ -1,6 +1,7 @@
 package connections
 
 import (
+	"context"
 	"net"
 	"syscall"
 	"testing"
@@ -8,6 +9,7 @@ import (
 )
 
 func DialTimeout(network string, address string, timeout time.Duration) (net.Conn, error) {
+
 	d := net.Dialer{
 		Control: func(_, address string, _ syscall.RawConn) error {
 			return &net.DNSError{
@@ -34,5 +36,44 @@ func TestDialTimeout(t *testing.T) {
 	}
 	if !nErr.Timeout() {
 		t.Fatal("Error is not a timeout")
+	}
+}
+
+func TestDialContext(t *testing.T) {
+	// Create a deadline to wait for.
+	dl := time.Now().Add(5 * time.Second)
+
+	// Create a context that is both manually cancellable and will signal
+	ctx, cancel := context.WithDeadline(context.Background(), dl)
+	defer cancel()
+
+	//
+	d := net.Dialer{
+		Control: func(_, _ string, _ syscall.RawConn) error {
+			// Sleep long enough to reach the context's deadline.
+			time.Sleep(5*time.Second + time.Millisecond)
+			return nil
+		},
+	}
+
+	conn, err := d.DialContext(ctx, "tcp", "10.0.0.0:http")
+	if err == nil {
+		conn.Close()
+		t.Fatal("Connection did not time out")
+	}
+
+	// Check that the error is a timeout.
+	nErr, ok := err.(net.Error)
+	if !ok {
+		t.Error(err)
+	} else {
+		if !nErr.Timeout() {
+			t.Errorf("Error is not a timeout: %v", err)
+		}
+	}
+
+	// Check that the context has timed out.
+	if ctx.Err() != context.DeadlineExceeded {
+		t.Errorf("Expected deadline exceeded; actual: %v", ctx.Err())
 	}
 }
